171d4a820d681cf8649e1e5f451ffa74e878dbc8
Lenovo <Lenovo@DESKTOP-D0OIE70>
2019-09-24 03:44:37 +0300

    Add project files.

---
+++ 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hisham alatrash");
+
+            Console.WriteLine("my name is hisham alatrash");
+            Console.WriteLine("console.writeline");
+            Console.ReadKey();
+            
+        }
+    }
+}
